a5c1f89a06a445188a191bb628dfa5d546962af0
Akshay Singla <akshay.singla27@gmail.com>
2016-06-07 23:46:55 +0530

    Modularized and Database added

--- ytDownloader.py
+++ ytDownloader.py
@@ -4,57 +4,83 @@ from pprint import pprint
 import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 import sys
-
-# yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
-
-# # pprint(yt.get_videos())
-# video = yt.get('3gp', '144p')
-
-# video.download('Desktop'+yt.filename+'.mp4');
-
-link = ""
-counter = 0
-if len(sys.argv) == 1:
-    link = input("URL of the youtube video to start: ")
-    counter_ = input("No. of Videos to download: ")
-elif len(sys.argv) == 3:
-    link = sys.argv[1]
-    counter_ = sys.argv[2]
-else:
-    raise ValueError("Incorrect initilization: Correct Usage is \n Either give no arguements or give link of the video followed by the no. of videos to download.")
-    sys.exit()
+import os
+import sqlite3
 
 
-try:
-   counter = int(counter_)
-except ValueError:
-   print("Counter must be an integer")
-    
-print("Starting Video : " + link )
-print("Count : " + str(counter))
+def insert_in_database(database, f) :
+    cursor = database.execute("SELECT name FROM SONGS WHERE name = (?)", (f,))
+    data = cursor.fetchone()
+    if data is None :
+        database.execute("INSERT INTO SONGS VALUES (?);", (f,))
+        database.commit() # Can do a commit before closing too.
+        return 1
+    else :
+        return 0
 
-http = httplib2.Http()
-status, response = http.request(link)
+def create_database(database,r) :
+    database.execute('''CREATE TABLE IF NOT EXISTS SONGS (NAME TEXT NOT NULL);''')
+    if r :
+        files = os.listdir("../Songs/")
+        for f in files:
+            insert_in_database(database, os.path.splitext(f)[0])
 
-print("Connection Established")
-while counter:
-    print(counter)
-    # Download the video to which link points
-    yt = YouTube(link);
-    print("Downloading : " + yt.filename);   
-    # video = yt.get('mp4', '720p')
-    # video.download('Desktop'+yt.filename+'.mp4');
-    ###########Downloading Finished. Prepare for the next step#############################
-    # Parse the HTML received 
-    soup = BeautifulSoup(response,"lxml")
-    # Select the div containg the link to the next video
-    div = soup.findAll('div', class_="autoplay-bar");      
-    div = str(div);
-    link_start_pos = div.find("href");
-    link ='https://www.youtube.com' +  div[link_start_pos+6 : link_start_pos+26];
+def download_video(http, link, database) :
     status, response = http.request(link)
-    counter -= 1;
+    curr_video_obj = YouTube(link)
+    print("Downloading : " + curr_video_obj.filename);
+
+    # Check if exits and add to database if it doesnt. Returns 1 if new video
+    to_download  = insert_in_database(database, curr_video_obj.filename)
+    # if to_download :
+    #     video = curr_video_obj.get('mp4', '720p')
+    #     video.download(curr_video_obj.filename+'.mp4')
+
+    soup = BeautifulSoup(response,"lxml")
+
+    # Select the div containg the link to the next video
+    div = soup.findAll('div', class_="autoplay-bar")
+    div = str(div)
+    link_start_pos = div.find("href")
+    return ('https://www.youtube.com' +  div[link_start_pos+6 : link_start_pos+26])
+
+def download_videos(http, link, database, counter) :
+    while counter:
+        print(counter)
+        link = download_video(http,link, database)
+        print(link)
+        counter -= 1;
 
 
+def main() :
+    link = ""
+    counter = 0
+    if len(sys.argv) == 1:
+        link = input("URL of the youtube video to start: ")
+        counter_ = input("No. of Videos to download: ")
+    elif len(sys.argv) == 3:
+        link = sys.argv[1]
+        counter_ = sys.argv[2]
+    else:
+        raise ValueError("Incorrect initilization: Correct Usage is \n Either give no arguements or give link of the video followed by the no. of videos to download.")
+        sys.exit()
 
-print("Script finished")
+    try:
+       counter = int(counter_)
+    except ValueError:
+       print("Counter must be an integer")
+
+    print("Starting Video : " + link )
+    print("Count : " + str(counter))
+
+    http = httplib2.Http()
+
+    conn = sqlite3.connect('downloadedsongs.db')
+    create_database(conn,0)
+
+    download_videos(http,link, conn, counter)
+
+    conn.close()
+    print("Script finished")
+
+main()
