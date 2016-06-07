97ba821aadbe807781bb8d700d56de5a9c155a28
Akshay Singla <akshay.singla27@gmail.com>
2016-06-07 22:24:25 +0530

    Test

--- ytDownloader.py
+++ ytDownloader.py
@@ -4,7 +4,6 @@ from pprint import pprint
 import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 
-
 # yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
 
 # # pprint(yt.get_videos())
@@ -23,14 +22,14 @@ while counter:
     print(counter)
     link="";
     soup = BeautifulSoup(response,"lxml")
-    div = soup.findAll('div', class_="autoplay-bar");      
+    div = soup.findAll('div', class_="autoplay-bar");
     div = str(div);
     a = div.find("href");
     # print(a)
     # print(div[a+6:a+26])
     link ='https://www.youtube.com' +  div[a+6:a+26];
     yt = YouTube(link);
-    print(yt.filename);    
+    print(yt.filename);
     status, response = http.request(link)
     counter -= 1;
 
